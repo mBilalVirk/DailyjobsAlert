7ffaee0fe50f6e5701ef783115d459f842eb0d02
mBilalVirk <bilalvirk337@gmail.com>
2024-08-10 16:32:29 +0500

    all data

--- logincheck.php
+++ logincheck.php
@@ -1,32 +1,21 @@
 <?php
-	require("Connection.php");
+require("Connection.php");
 
-if(isset($_POST['submit']))
-    {	
-        $login = false;
-        $id = $_POST['id'];
-		$password =  $_POST['password'];
-        $sql = "SELECT * FROM `account` WHERE `email`='$id' AND `password`='$password';";
-		$q=mysqli_query($dbc, $sql);
-        $row = mysqli_num_rows($q);
-        if($row==1)
-        {   
-			$login = true;
-            $_SESSION['id']=$id;
-		header('location:index.php');
-		}
-        else
-        {
-				
-        $_SESSION["error"]=$error;
-		header('location:login.php');
-            
-       
-        }
-										
-            
-       
+if (isset($_POST['submit'])) {
+    $login = false;
+    $id = $_POST['id'];
+    $password =  $_POST['password'];
+    $sql = "SELECT * FROM `account` WHERE `email`='$id' AND `password`='$password';";
+    $q = mysqli_query($dbc, $sql);
+    $row = mysqli_num_rows($q);
 
+    if ($row == 1) {
+        $login = true;
+        $_SESSION['id'] = $id;
+        header('location:index.php');
+    } else {
+
+        $_SESSION["error"] = $error;
+        header('location:login.php');
     }
-							 	
-?>
+}
